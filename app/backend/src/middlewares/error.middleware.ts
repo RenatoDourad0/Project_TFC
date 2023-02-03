3ccf5bd55486bb7bc135aa8b1f173edfa32a874b
RenatoDourad0 <renatocvdourado@hotmail.com>
2023-02-03 11:09:08 -0300

    refatoração error middleware e httpExceptions personalisadas

--- app/backend/src/middlewares/error.middleware.ts
+++ app/backend/src/middlewares/error.middleware.ts
@@ -1,3 +1,4 @@
+import 'dotenv/config';
 import { Request, Response, NextFunction } from 'express';
 import HttpException from '../errors/HttpException';
 
@@ -7,9 +8,11 @@ function errorMiddleware(
   res: Response,
   _next: NextFunction,
 ) {
-  console.error(`Status: ${err.status} --- Message: ${err.message} 
-  ----
-  ${err.stack}`);
+  if (process.env.node_env !== 'production') {
+    console.error(`Status: ${err.status} --- Message: ${err.message} 
+    ----
+    ${err.stack}`);
+  }
   return res.status(err.status || 500).json({ message: err.message });
 }
 

65f7e552d0eff44869275398bf9f35b26900298e
RenatoDourad0 <renatocvdourado@hotmail.com>
2023-02-27 10:14:34 -0300

    fix: cors config

--- app/frontend/src/services/requests.js
+++ app/frontend/src/services/requests.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: `https://projecttfcback-production.up.railway.app`,
+  baseURL: `https://projecttfcback-production.up.railway.app:6367`,
 });
 
 export const setToken = (token) => {

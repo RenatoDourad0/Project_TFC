cd451c86d009a6ed1ea3dd91702ecca11ce89d23
RenatoDourad0 <renatocvdourado@hotmail.com>
2023-02-27 09:47:34 -0300

    fix: cors config

--- app/backend/src/app.ts
+++ app/backend/src/app.ts
@@ -25,6 +25,7 @@ class App {
     }))
 
     this.app.use(helmet());
+    
     const accessControl: express.RequestHandler = (_req, res, next) => {
       res.header('Access-Control-Allow-Origin', '*');
       res.header('Access-Control-Allow-Methods', 'GET,POST,DELETE,OPTIONS,PUT,PATCH');

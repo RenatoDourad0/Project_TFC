5a8eeb19aa238da33de1298c4915ec64af17ed4a
RenatoDourad0 <renatocvdourado@hotmail.com>
2023-02-27 11:03:32 -0300

    fix: cors config

--- app/backend/src/server.ts
+++ app/backend/src/server.ts
@@ -1,6 +1,6 @@
 import { App } from './app';
 import 'dotenv/config';
 
-const PORT = process.env.BACKPORT || 3000;
+const PORT = process.env.BACKPORT || 3001;
 
-new App().start(PORT);
+new App().start(PORT); 

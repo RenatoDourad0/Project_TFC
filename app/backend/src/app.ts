b9fbc37913573e459c1a4c1b0058f220cc78febc
RenatoDourad0 <renatocvdourado@hotmail.com>
2023-02-27 09:00:02 -0300

    fix: cors config

--- app/backend/src/app.ts
+++ app/backend/src/app.ts
@@ -1,7 +1,6 @@
 import * as express from 'express';
 import helmet from 'helmet';
 import rateLimit from 'express-rate-limit';
-import cors from 'cors';
 import { errorMiddleware } from './middlewares';
 import { loginRouter, teamRouter, matchRouter, leaderBoardRouter } from './routes';
 
@@ -18,8 +17,6 @@ class App {
   }
 
   private config():void {
-    this.app.use(cors());
-
     this.app.use(rateLimit({
       windowMs: 15 * 60 * 1000, // 15 minutes
       max: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
